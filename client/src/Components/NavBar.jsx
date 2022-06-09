d8d407840e6fe8c6c355c033a7cfd7dac3cc057e
John Chia <john.chia0106@gmail.com>
2022-06-09 00:10:10 -0700

    add dependencies for packages

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,56 +1,60 @@
 import React, { useState } from "react";
 import {
   AppBar,
-  Button,
-  Tab,
-  Tabs,
+  Box,
   Toolbar,
+  IconButton,
   Typography,
-  useMediaQuery,
-  useTheme,
+  Menu,
+  Container,
+  Avatar,
+  Button,
+  Tooltip,
+  MenuItem,
+  InputBase,
+  Stack,
+  FormControlLabel,
+  Switch,
+  Tabs,
+  Tab,
 } from "@mui/material";
-import PetsIcon from '@mui/icons-material/Pets';
+import PetsIcon from "@mui/icons-material/Pets";
+import { Link } from "react-router-dom";
+import { styled, alpha } from '@mui/material/styles';
 
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 const Navigation = () => {
-  const [value, setValue] = useState();
-  const theme = useTheme();
-  console.log('theme: ', theme)
-  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+  const [value, setValue] = useState(0);
 
   return (
     <React.Fragment>
-      <AppBar sx={{ background: "#494949" }} position='relative'>
+      <AppBar sx={{ background: "#494949" }} position="relative">
         <Toolbar>
           <PetsIcon sx={{ transform: "scale(1)" }} />
-          {isMatch ? (
-            <>
-              <Typography sx={{ fontSize: "1rem", paddingLeft: "15px" }}>
-                Walkie Doggie
-              </Typography>
-          
-            </>
-          ) : (
-            <>
-              <Tabs
-                sx={{ marginLeft: "15px" }}
-                indicatorColor="secondary"
-                textColor="inherit"
-                value={value}
-                onChange={(e, value) => setValue(value)}
-              >
-                <Tab label="Home" />
-                <Tab label="About Us" />
-                <Tab label="Contact Us" />
-              </Tabs>
-              <Button sx={{ marginLeft: "auto" }} variant="contained">
-                Login
-              </Button>
-              <Button sx={{ marginLeft: "10px" }} variant="contained">
-                SignUp
-              </Button>
-            </>
-          )}
+
+          <Tabs
+            sx={{ marginLeft: "15px" }}
+            indicatorColor="secondary"
+            textColor="inherit"
+            value={value}
+            onChange={(e, value) => setValue(value)}
+          >
+            <Tab label="Home" {...a11yProps(0)} />
+            <Tab label="About Us" {...a11yProps(1)} />
+            <Tab label="Contact Us" {...a11yProps(2)} />
+          </Tabs>
+          <Button sx={{ marginLeft: "auto" }} variant="contained">
+            Login
+          </Button>
+          <Button sx={{ marginLeft: "10px" }} variant="contained">
+            SignUp
+          </Button>
         </Toolbar>
       </AppBar>
     </React.Fragment>
