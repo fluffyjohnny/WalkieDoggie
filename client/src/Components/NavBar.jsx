e01971ccb25f2304fb6df224a90189a290a32f13
John Chia <john.chia0106@gmail.com>
2022-06-09 17:04:33 -0700

    create hamberger menu when vp is sx

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -19,8 +19,9 @@ import {
   Tab,
 } from "@mui/material";
 import PetsIcon from "@mui/icons-material/Pets";
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from "react-router-dom";
-import { styled, alpha } from '@mui/material/styles';
+import { styled, alpha } from "@mui/material/styles";
 
 function a11yProps(index) {
   return {
@@ -29,17 +30,158 @@ function a11yProps(index) {
   };
 }
 
+const pageLinks = [{ text: "Browse", route: "/" }];
+
+const authPageLinks = [
+  { text: "Cart", route: "/orders/cart" },
+  { text: "Previous Orders", route: "/orders/previous" },
+];
+
+const vendorPageLinks = [
+  { text: "Current Orders", route: "/orders/vendor" },
+  { text: "Add New Dish", route: "/dishes/new" },
+];
+
 const Navigation = () => {
   const [value, setValue] = useState(0);
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = event => {
+    setAnchorElNav(event.currentTarget);
+  };
+  // const handleOpenUserMenu = event => {
+  //   setAnchorElUser(event.currentTarget);
+  // };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  // const handleCloseUserMenu = () => {
+  //   setAnchorElUser(null);
+  // };
+
+  // const handleLogin = (e, input) => {
+  //   setAnchorElNav(null);
+  //   setAnchorElUser(null);
+
+  //   // delay login to allow for animation to catch up with state
+  //   setTimeout(() => {
+  //     login(e, input);
+  //   }, 500);
+  // };
+
+  // const handleLogout = () => {
+  //   setAnchorElUser(null);
+  //   setAnchorElNav(null);
+
+  //   // delay logout to allow for animation to catch up with state
+  //   setTimeout(() => {
+  //     logout();
+  //   }, 500);
+  // };
 
   return (
-    <React.Fragment>
-      <AppBar sx={{ background: "#494949" }} position="relative">
-        <Toolbar>
-          <PetsIcon sx={{ transform: "scale(1)" }} />
+    <AppBar sx={{ background: "#494949" }} position="relative">
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <PetsIcon sx={{display: { xs: "none", md: "flex" }  }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, ml: 2, display: { xs: "none", md: "flex" } }}
+          >
+            WalkieDoggie
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pageLinks.map((page) => (
+                <Link
+                  key={page.text}
+                  onClick={handleCloseNavMenu}
+                  component={Typography}
+                  to={page.route}
+                  style={{
+                    textDecoration: "none",
+                    fontSize: "larger",
+                    color: "#111",
+                  }}
+                >
+                  <MenuItem onClick={handleCloseNavMenu}>{page.text}</MenuItem>
+                </Link>
+              ))}
+              {/* map over page routes to be shown if user is logged in */}
+              {/* {userId &&
+                authPageLinks.map((page) => (
+                  <Link
+                    key={page.text}
+                    onClick={handleCloseNavMenu}
+                    component={Typography}
+                    to={page.route}
+                    style={{
+                      textDecoration: "none",
+                      fontSize: "larger",
+                      color: "#111",
+                    }}
+                  >
+                    <MenuItem onClick={handleCloseNavMenu}>
+                      {page.text}
+                    </MenuItem>
+                  </Link>
+                ))} */}
+              {/* map over page routes to be shown if user is logged in and is a vendor */}
+              {/* {isVendor &&
+                vendorPageLinks.map((page) => (
+                  <Link
+                    key={page.text}
+                    onClick={handleCloseNavMenu}
+                    component={Typography}
+                    to={page.route}
+                    style={{
+                      textDecoration: "none",
+                      fontSize: "larger",
+                      color: "#111",
+                    }}
+                  >
+                    <MenuItem onClick={handleCloseNavMenu}>
+                      {page.text}
+                    </MenuItem>
+                  </Link>
+                ))} */}
+            </Menu>
+          </Box>
 
           <Tabs
-            sx={{ marginLeft: "15px" }}
             indicatorColor="secondary"
             textColor="inherit"
             value={value}
@@ -56,8 +198,8 @@ const Navigation = () => {
             SignUp
           </Button>
         </Toolbar>
-      </AppBar>
-    </React.Fragment>
+      </Container>
+    </AppBar>
   );
 };
 
