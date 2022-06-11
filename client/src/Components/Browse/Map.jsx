971230109d8c2ce069dad6e00de15b96bd3a9d9d
John Chia <john.chia0106@gmail.com>
2022-06-10 21:39:15 -0700

    wrap in card component and resize\

--- client/src/Components/Browse/Map.jsx
+++ client/src/Components/Browse/Map.jsx
@@ -1,11 +1,11 @@
 import React, { Fragment, useState, useEffect } from "react";
-import { Typography } from "@mui/material";
+import { Card } from "@mui/material";
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 const containerStyle = {
   width: "100%",
   height: "100%",
-  minHeight: '55vh',
+  minHeight: "66vh",
 };
 
 const center = {
@@ -15,12 +15,26 @@ const center = {
 
 function Map() {
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GMAPS_APIKEY}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
-        {/* Child components, such as markers, info windows, etc. */}
-        <></>
-      </GoogleMap>
-    </LoadScript>
+    <Card
+      sx={{
+        padding: "15px",
+        borderColor: "primary.main",
+        backgroundColor: "info.main",
+      }}
+    >
+      <Card>
+        <LoadScript googleMapsApiKey={process.env.REACT_APP_GMAPS_APIKEY}>
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={12}
+          >
+            {/* Child components, such as markers, info windows, etc. */}
+            <></>
+          </GoogleMap>
+        </LoadScript>
+      </Card>
+    </Card>
   );
 }
 
