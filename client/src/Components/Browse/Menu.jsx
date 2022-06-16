b6598f08d5164455ceec0aa5be20a8b0728d743c
John Chia <john.chia0106@gmail.com>
2022-06-15 23:08:10 -0700

    add css to menu selection

--- client/src/Components/Browse/Menu.jsx
+++ client/src/Components/Browse/Menu.jsx
@@ -1,60 +1,126 @@
-import React from 'react';
-import Container from '@mui/material/Container';
-
+import React from "react";
+import {
+  Container,
+  Card,
+  Stack,
+  Button,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+  Grid,
+  FormControl,
+  InputLabel,
+  TextField,
+  Select,
+  MenuItem,
+} from "@mui/material";
 
 const Menu = () => {
+  const [pets, setPets] = React.useState(() => []);
+
+  const handlePets = (event, pet) => {
+    setPets(pet);
+  };
+
+  const [service, setService] = React.useState("");
+
+  const handleService = (event) => {
+    setService(event.target.value);
+  };
+
+  const [size, setSize] = React.useState("");
+
+  const handleSize = (event) => {
+    setSize(event.target.value);
+  };
 
   return (
-    <Container
+    <Card
       sx={{
-        width: 300,
-        height: 300,
-        backgroundColor: 'primary',
-        '&:hover': {
-          backgroundColor: 'primary',
-          opacity: [0.9, 0.8, 0.7],
-        },
+        borderColor: "primary.main",
+        mb: 2,
+        backgroundColor: "info.main",
+        py: 1,
       }}
     >
-    <div className='menu-container'>
-      <form>
-      Im looking for service for my...
-      <input type='checkbox' />
-      <label>Dog</label>
-      <input type='checkbox' />
-      <label>Cat</label>
+      <Container sx={{ margin: "20px 0" }}>
+        <Stack direction="column" spacing={2}>
+          <Grid container spacing={3} justify="center" alignItems="center" sx={{mb: "20px"}}>
+            <Grid item>
+              <Typography>Im looking for service for my...</Typography>
+            </Grid>
+            <Grid item>
+              <ToggleButtonGroup
+                color="warning"
+                size="small"
+                value={pets}
+                onChange={handlePets}
+                arial-label="pet selection"
+              >
+                <ToggleButton value="dog" aria-label="dog">
+                  Dog
+                </ToggleButton>
+                <ToggleButton value="cat" aria-label="cat">
+                  Cat
+                </ToggleButton>
+                <ToggleButton value="bunny" aria-label="cat">
+                  Bunny
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Grid>
+          </Grid>
+          <FormControl fullWidth>
+            <InputLabel>I'm looking for...</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={service}
+              label="I'm looking for..."
+              onChange={handleService}
+            >
+              <MenuItem value={"boarding"}>Boarding</MenuItem>
+              <MenuItem value={"drop-in"}>Drop-In Visit</MenuItem>
+              <MenuItem value={"house sitting"}>House Sitting</MenuItem>
+              <MenuItem value={"doggy daycare"}>Doggy Daycare</MenuItem>
+              <MenuItem value={"dog walking"}>Dog Walking</MenuItem>
+            </Select>
+          </FormControl>
 
-      <br />
-      
-      <select>
-        <option>I need...</option>
-        <option>Boarding</option>
-        <option>Drop-In Visits</option>
-        <option>House Sitting</option>
-        <option>Doggy Day Care</option>
-        <option>Dog Walking</option>
-      </select>
-      
-      
-      
-      <label>location</label>
-      <input></input>
-
-      <label>Dates</label>
-      <input type='date' ></input> to <input type='date' ></input>
-
-      <label>my pet size</label>
-      <select>
-        <option> &lt; 10 lb </option>
-        <option> 10 - 15 lb </option>
-        <option> 15 - 20 lb </option>
-        <option> &gt; 0 lb </option>
-      </select>
-      </form>
-      <button>submit</button>
-    </div>
-    </ Container>
-  )
-}
+          <InputLabel>Dates</InputLabel>
+          <TextField
+            required
+            id="date-from"
+            label="From"
+            placeholder="yyyy-mm-dd"
+          />
+          <TextField
+            required
+            id="date-to"
+            label="To"
+            placeholder="yyyy-mm-dd"
+          />
+          <InputLabel>My pet's size</InputLabel>
+          <FormControl fullWidth>
+          <InputLabel>lbs</InputLabel>
+          <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={size}
+              label="lbs"
+              onChange={handleSize}
+              sx={{mb: "10px"}}
+            >
+              <MenuItem value={"boarding"}>&lt; 10</MenuItem>
+              <MenuItem value={"drop-in"}>10 - 15</MenuItem>
+              <MenuItem value={"house sitting"}>15 - 20</MenuItem>
+              <MenuItem value={"doggy daycare"}>&gt; 20</MenuItem>
+            </Select>
+            </FormControl>
+          <Button variant="contained" type='submit'>Submit</Button>
+        </Stack>
+      </Container>
+    </Card>
+  );
+};
 
 export default Menu;
